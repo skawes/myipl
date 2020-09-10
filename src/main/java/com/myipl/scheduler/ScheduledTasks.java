fe8d14113b34dcedb58b1ffb147f116644159858
STS\awes.shaikh <awes.shaikh@sts.in>
2020-09-10 23:11:10 +0530

    Added the compute leaderboard using scheduled annotation

---
+++ src/main/java/com/myipl/scheduler/ScheduledTasks.java
@@ -0,0 +1,29 @@
+package com.myipl.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.myipl.service.LeaderboardService;
+
+@Component
+public class ScheduledTasks {
+	private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+
+	@Autowired
+	private LeaderboardService leaderboardService;
+
+	/**
+	 * This method is scheduled to compute the leader board at 2 a.m everyday
+	 */
+	@Scheduled(cron = "0 0 2 * * ?")
+	public void computeLeaderBoard() {
+		try {
+			leaderboardService.computeLeaderBoard();
+		} catch (Exception e) {
+			logger.error("Exception executing LeaderBoard Job : " + e.getMessage(), e);
+		}
+	}
+}
